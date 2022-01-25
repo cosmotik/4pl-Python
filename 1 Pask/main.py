f63f09da11bf32de1cfe6c59457da30525ed829c
cosmotik <martynas.rusakas@gmail.com>
2022-01-25 19:55:01 +0200

    1 pask

---
+++ 1 Pask/main.py
@@ -0,0 +1,28 @@
+# print("Hello World")
+# print(type("swd"))
+my_name = "Martynas"
+living_place = "Vilnius"
+hobby = "futbolas, programavimas, elektronika"
+age = 18
+
+print(f"""
+My name is: {my_name}
+My age is: {age}
+My hobbies {hobby.lower()}""")
+
+print(f"My name is: {my_name}\nMy age is: {age}\nMy hobbies: {hobby.lower()}")
+
+#print("My name is: " + my_name)
+#print("Age: " + age)
+#print("Living in: " + living_place)
+#print("hobbies: " + hobby)
+
+#print("My name is: ", my_name)
+#print("Age: ", age)
+#print("Living in: ", living_place)
+#print("hobbies: ", hobby)
+
+#print(f"My name is: ", {my_name})
+#print(f"Age: ", {age})
+#print(f"Living in: ", {living_place})
+#print(f"hobbies: ", {hobby})
