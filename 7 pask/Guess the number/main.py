dcec9bd605682899f4608abcbd009374073b87f3
Martynas Rusakas <martynas.rusakas@gmail.com>
2022-02-15 19:58:47 +0200

    Update main.py

--- 7 pask/Guess the number/main.py
+++ 7 pask/Guess the number/main.py
@@ -1,30 +1,77 @@
 import random
 
-From = int(input("Input the lower value: "))
-To = int(input("Input the Upper value: "))
-guesses = 0
-difficulty_tries = {'Easy': 9, 'Medium': 6, 'Hard': 3}
+class GuessNumber:
+    number = None
+    randomize_number = None
+    life = None
+    victory = False
+    level = {'easy': 9, 'normal': 6, 'hard': 3}
 
-while True:
-    difficulty = input("Choose (Easy/Medium/Hard) difficulty")
-    difficulty_tries = input(f"You have {difficulty_tries[difficulty]} tries")
-    try:
-        number = random.randint(From, To)
-        break
-    except KeyError:
-        print("Incorrect input, try again!")
-while True:
-        guess = int(input("Your number: "))
-        guesses += 1
-        if number == guess:
-            print(f"Correct guess! You used {guesses} guesses")
-        elif guess < From or guess > To:
-            print("Your guess is out of range!!!")
-        elif guess > number:
-            print("Number is too high")
-        elif guess < number:
-            print("Number is too low")
 
-        if guesses >= int(difficulty_tries[int(difficulty)]):
-            print("You dont have more guesses")
-            print("The number was", number)
+    def radomize_number(self):
+        """
+        Generate random number
+        :return: random_number
+        """
+        self.randomize_number = random.randrange(1, 16)
+
+
+    def player_input_number(self):
+        """
+        Player input number
+        :return:
+        """
+        self.number = int(input('Input number from 1 to 15: '))
+
+
+    def number_of_lives(self):
+        """
+        Case  for level choose
+        :return: self.life
+        """
+        while True:
+            number = int(input('Choose your level (1 - easy / 2 - normal / 3 - hard): '))
+            if number == 1:
+                self.life = self.level['easy']
+                print(f'Chosen level - easy, you have {self.level["easy"]} life\'s')
+                return self.life
+            elif number == 2:
+                self.life = self.level['normal']
+                print(f'Chosen level - normal, you have {self.level["normal"]} lives')
+                return self.life
+            elif number == 3:
+                self.life = self.level['hard']
+                print(f'Chosen level - hard, you have {self.level["hard"]} lives')
+                return self.life
+            else:
+                print('You input wrong number! Please repeat input!')
+
+
+    def game_logic(self, life):
+        count = 1
+        self.radomize_number()
+        print(f'You have {life} try\'s')
+        while count <= life:
+            print(f'Try number {count}')
+            self.player_input_number()
+            if self.number > self.randomize_number:
+                print('The number is higher than hidden number!')
+                count += 1
+                continue
+            elif self.number < self.randomize_number:
+                print('The number is less than hidden number!')
+                count += 1
+                continue
+            else:
+                print(f'You guessed the hidden number: {self.randomize_number}')
+                self.victory = True
+                break
+        if self.victory:
+            print(f'Congratulation!!! You win!!!')
+        else:
+            print(f'You loose, try another time!!!')
+
+
+gn = GuessNumber()
+life = gn.number_of_lives()
+gn.game_logic(life)
